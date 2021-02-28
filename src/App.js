8c05948fbeb1b5927b8d6cbc44ab787df709f912
Atsushi Ishida <gipcompany@gmail.com>
2021-02-28 21:13:37 +0900

    import js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
+import React, { useEffect } from 'react';
+
 function App() {
+  useEffect(() => {
+    const boxes = document.querySelectorAll('.box');
+
+    window.addEventListener('scroll', checkBoxes);
+
+    checkBoxes();
+    function checkBoxes() {
+      const triggerBottom = (window.innerHeight / 5) * 4;
+      boxes.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < triggerBottom) {
+          console.log('foo');
+          box.classList.add('show');
+        } else {
+          box.classList.remove('show');
+        }
+      });
+    }
+  }, []);
+
   return (
     <div>
       <h1>スクロールしてね</h1>
