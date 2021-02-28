a89fc0b724154681ed6a46d94a5288035c546288
Atsushi Ishida <gipcompany@gmail.com>
2021-02-28 21:11:19 +0900

    rename class to className

--- src/App.js
+++ src/App.js
@@ -2,58 +2,58 @@ function App() {
   return (
     <div>
       <h1>スクロールしてね</h1>
-      <div class="box">
+      <div className="box">
         <h2>Good Morning 😊</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>こたろうです</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>今日も元気〜🐈</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>Good Morning 😊</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>こたろうです</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>今日も元気〜</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>Good Morning 😊</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>こたろうです</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>今日も元気〜</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>Good Morning 😊</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>こたろうです</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>今日も元気〜🐈</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>Good Morning 😊</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>こたろうです</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>今日も元気〜🐈</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>Good Morning 😊</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>こたろうです</h2>
       </div>
-      <div class="box">
+      <div className="box">
         <h2>今日も元気〜🐈</h2>
       </div>
     </div>
