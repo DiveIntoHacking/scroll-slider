95040f41f6348700da56b09a0c7ec557b26e7fe5
Atsushi Ishida <gipcompany@gmail.com>
2021-02-28 21:10:13 +0900

    change html, but class should be changed with className

--- src/App.js
+++ src/App.js
@@ -1,5 +1,63 @@
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>スクロールしてね</h1>
+      <div class="box">
+        <h2>Good Morning 😊</h2>
+      </div>
+      <div class="box">
+        <h2>こたろうです</h2>
+      </div>
+      <div class="box">
+        <h2>今日も元気〜🐈</h2>
+      </div>
+      <div class="box">
+        <h2>Good Morning 😊</h2>
+      </div>
+      <div class="box">
+        <h2>こたろうです</h2>
+      </div>
+      <div class="box">
+        <h2>今日も元気〜</h2>
+      </div>
+      <div class="box">
+        <h2>Good Morning 😊</h2>
+      </div>
+      <div class="box">
+        <h2>こたろうです</h2>
+      </div>
+      <div class="box">
+        <h2>今日も元気〜</h2>
+      </div>
+      <div class="box">
+        <h2>Good Morning 😊</h2>
+      </div>
+      <div class="box">
+        <h2>こたろうです</h2>
+      </div>
+      <div class="box">
+        <h2>今日も元気〜🐈</h2>
+      </div>
+      <div class="box">
+        <h2>Good Morning 😊</h2>
+      </div>
+      <div class="box">
+        <h2>こたろうです</h2>
+      </div>
+      <div class="box">
+        <h2>今日も元気〜🐈</h2>
+      </div>
+      <div class="box">
+        <h2>Good Morning 😊</h2>
+      </div>
+      <div class="box">
+        <h2>こたろうです</h2>
+      </div>
+      <div class="box">
+        <h2>今日も元気〜🐈</h2>
+      </div>
+    </div>
+  );
 }
 
 export default App;
